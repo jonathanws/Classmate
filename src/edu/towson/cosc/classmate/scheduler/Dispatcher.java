917e0eea0ee68cc7ad7b1613f0356ab8a4762e14
GeneralZero <generaljeezy@yahoo.com>
2014-04-24 01:35:27 -0400

    Completed most of the rough class implementations. Waiting to test everything and ensure their functionality, scheduling, performance, access, etc.
    Dispatcher class added to design and implemented.
    
    System interface no longer talks to Invoker. SystemCall objects do.

---
+++ src/edu/towson/cosc/classmate/scheduler/Dispatcher.java
@@ -0,0 +1,40 @@
+package edu.towson.cosc.classmate.scheduler;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Dispatcher implements Runnable {
+	
+	private ExecutorService dispatcher = Executors.newSingleThreadExecutor();
+	private Thread runner = new Thread( this, "Dispatcher" );
+	
+	public void run() {
+		MultilevelQueue queue = Scheduler.getQueue();
+		
+		while( queue.getCount() > 0 ) {
+			SystemCall next = queue.nextCommand();
+			
+			dispatcher.execute( next );
+			
+			next.join();
+		}
+	}
+	
+	// For "fun" method (can implement if bored)
+	public void preempt( SystemCall task ) {
+		
+	}
+	
+	public void join() {
+		try {
+			this.runner.join();
+		} catch( InterruptedException error ) {
+			error.printStackTrace();
+		}
+	}
+	
+	public void start() {
+		this.runner.start();
+	}
+	
+}
